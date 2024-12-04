d36e0544ff93a31f438c11afb1a5b43174fedd92
Junior Morais <jrmorais3@gmail.com>
2024-12-04 05:11:20 -0300

    adding Data and Models folders

---
+++ dotnet-magic-reads/Data/DbContext/FabContext.cs
@@ -0,0 +1,12 @@
+﻿using dotnet_magic_reads.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_magic_reads.Data
+{
+    public class FabContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public FabContext(DbContextOptions<FabContext> options) : base(options) { }
+    }
+}
---
+++ dotnet-magic-reads/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace dotnet_magic_reads.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public string Category { get; set; }
+        public string Img { get; set; }
+    }
+}
