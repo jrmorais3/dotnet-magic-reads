24b77f3dc11bcf3a9ca7e1e547dd57430bdfea9a
Junior Morais <jrmorais3@gmail.com>
2024-12-04 05:50:42 -0300

    refactoting model class

--- dotnet-magic-reads/Models/Book.cs
+++ dotnet-magic-reads/Models/Book.cs
@@ -3,10 +3,10 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public int Stock { get; set; }
-        public string Category { get; set; }
-        public string Img { get; set; }
+        public required string Name { get; set; }
+        public required double Price { get; set; }
+        public required int Stock { get; set; }
+        public required string Category { get; set; }
+        public string? Img { get; set; }
     }
 }
