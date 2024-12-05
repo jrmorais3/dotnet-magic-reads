5b366db63805574751d11dc7329724cb822fb508
Junior Morais <jrmorais3@gmail.com>
2024-12-05 10:37:46 -0300

    adding JsonIgnore and Key on Id prop

--- dotnet-magic-reads/Models/Book.cs
+++ dotnet-magic-reads/Models/Book.cs
@@ -1,7 +1,14 @@
-﻿namespace dotnet_magic_reads.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace dotnet_magic_reads.Models
 {
     public class Book
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
         public int Id { get; set; }
         public required string Name { get; set; }
         public required double Price { get; set; }
